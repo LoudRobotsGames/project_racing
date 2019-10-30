66a17e9267b8da84c7d2455e85ade0e1ceee2eb0
VengantMjolnir <vengantmjolnir@gmail.com>
2019-10-29 20:16:49 -0700

    Commit of whatever on surface

--- Tiled/ground.tsx
+++ Tiled/ground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ground" tilewidth="128" tileheight="128" spacing="2" margin="0" tilecount="64" columns="8">
- <image source="ground.png" width="1040" height="1040"/>
+<tileset name="ground" tilewidth="128" tileheight="128" spacing="2" tilecount="64" columns="8">
+ <image source="../../../Unity/ggjam_2018/Tiled/ground.png" width="1040" height="1040"/>
 </tileset>
